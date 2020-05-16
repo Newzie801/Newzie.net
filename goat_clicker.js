c9e8ad6752e53ab2be58dfb825a1d7eddd38d36c
Newzie801 <thburnham@protonmail.com>
2020-05-16 01:39:03 -0600

    Added money mechanic to goat clicker

--- goat_clicker.js
+++ goat_clicker.js
@@ -1,24 +1,75 @@
-var gameContainerElement = document.getElementById('goat_game');
+var gameContainer = document.getElementById('goat_game');
 
-var gameHeadlineElement = document.createElement('h1');
-gameHeadlineElement.innerText = 'Goat Clicker';
+var gameHeadline = document.createElement('h1');
+gameHeadline.innerText = 'Goat Clicker';
+gameContainer.appendChild(gameHeadline);
 
-gameContainerElement.appendChild(gameHeadlineElement);
+var moneyDisplay = document.createElement('h2');
+gameContainer.appendChild(moneyDisplay)
+var moneyDescription = document.createElement('p');
+gameContainer.appendChild(moneyDescription)
+var money = 0;
+var updateMoneyView = function () {
+    var moneyMadeThisSecond = (
+        0
+        + (goatCount * moneyPerGoatPerSecond)
+    );
+    money += moneyMadeThisSecond;
+    moneyDisplay.innerText = (
+        'money: '
+        + money
+    );
+    moneyDescription.innerText = (
+        'Your goats generate '
+        + moneyMadeThisSecond
+        + ' money per second'
+    );
+}
+setInterval(
+    updateMoneyView,
+    1000
+);
 
-var gameButtonElement = document.createElement('button');
-gameButtonElement.innerText = 'add a goat';
-gameContainerElement.appendChild(gameButtonElement);
+var goatHeadline = document.createElement('h2');
+goatHeadline.innerText = 'Goats';
+gameContainer.appendChild(goatHeadline);
+var goatDescription = document.createElement('p');
+goatDescription.innerText = (
+    'Goats are great! Your goats automatically generate money for you.'
+    + ' Each goat gives you '
+    + moneyPerGoatPerSecond
+    + ' money per second.'
+);
+var moneyPerGoatPerSecond = 0.1;
 
-var gameClickCount = 0;
-var updateView = function () {
-    gameButtonElement.innerText = 'goat count: ' + gameClickCount;
+
+gameContainer.appendChild(goatDescription);
+var goatCountDisplay = document.createElement('p');
+gameContainer.appendChild(goatCountDisplay);
+var goatCount = 0;
+var updateGoatView = function () {
+    goatDescription.innerText = (
+        'Goats are great! Your goats automatically generate money for you.'
+        + ' Each goat gives you '
+        + moneyPerGoatPerSecond
+        + ' money per second.'
+        + 'Your goats are now giving you '
+        + (goatCount * moneyPerGoatPerSecond)
+        + ' money per second'
+    );
+    goatCountDisplay.innerText = 'goat count: ' + goatCount;
 };
-var gameHandleClick = function () {
-    gameClickCount += 1;
-    updateView();
+var addGoatButton = document.createElement('button');
+addGoatButton.innerText = 'add a goat';
+gameContainer.appendChild(addGoatButton);
+var addGoat = function () {
+    goatCount += 1;
+    updateGoatView();
 };
-gameButtonElement.addEventListener(
+addGoatButton.addEventListener(
     'click',
-    gameHandleClick,
+    addGoat,
     false
 );
+
+
