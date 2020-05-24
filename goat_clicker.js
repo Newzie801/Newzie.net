cfaefa8fd8488aaab0a246d6cadf0be7a5166721
Newzie801 <thburnham@protonmail.com>
2020-05-24 17:01:32 -0600

    added goat cost variable and test

--- goat_clicker.js
+++ goat_clicker.js
@@ -8,7 +8,7 @@ var moneyDisplay = document.createElement('h2');
 gameContainer.appendChild(moneyDisplay)
 var moneyDescription = document.createElement('p');
 gameContainer.appendChild(moneyDescription)
-var money = 0;
+var money = 10;
 var updateMoneyView = function () {
     var moneyMadeThisSecond = (
         0
@@ -34,19 +34,25 @@ var goatHeadline = document.createElement('h2');
 goatHeadline.innerText = 'Goats';
 gameContainer.appendChild(goatHeadline);
 var goatDescription = document.createElement('p');
+var moneyPerGoatPerSecond = 1;
 goatDescription.innerText = (
     'Goats are great! Your goats automatically generate money for you.'
     + ' Each goat gives you '
     + moneyPerGoatPerSecond
     + ' money per second.'
 );
-var moneyPerGoatPerSecond = 0.1;
 
+var goatDescription2 = document.createElement('p')
+goatDescription2.innerText = (
+    'Each goat will cost 10 money.  Click the hire a goat button below to start generating money.'
+);
 
 gameContainer.appendChild(goatDescription);
+gameContainer.appendChild(goatDescription2)
 var goatCountDisplay = document.createElement('p');
 gameContainer.appendChild(goatCountDisplay);
 var goatCount = 0;
+var goatCost = 10;
 var updateGoatView = function () {
     goatDescription.innerText = (
         'Goats are great! Your goats automatically generate money for you.'
@@ -60,11 +66,21 @@ var updateGoatView = function () {
     goatCountDisplay.innerText = 'goat count: ' + goatCount;
 };
 var addGoatButton = document.createElement('button');
-addGoatButton.innerText = 'add a goat';
+addGoatButton.innerText = 'hire a goat';
 gameContainer.appendChild(addGoatButton);
 var addGoat = function () {
-    goatCount += 1;
-    updateGoatView();
+    if (money >= goatCost) {
+        goatCount += 1;
+        money -= goatCost;
+        updateGoatView();
+    } else {
+        alert(
+            'you do not have enough money to hire a goat! ...yet.'
+            + ' you need '
+            + (goatCost - money)
+            + ' more money to hire a goat'
+        );
+    }
 };
 addGoatButton.addEventListener(
     'click',
